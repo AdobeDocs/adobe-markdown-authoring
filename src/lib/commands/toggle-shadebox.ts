969ae73cb7668a14708701cb921fd0fab5173330
George Everitt <geveritt@appliedrelevance.com>
2023-04-13 22:48:05 -0400

    Mostly works now.  Checkpoint.

--- src/lib/commands/toggle-shadebox.ts
+++ src/lib/commands/toggle-shadebox.ts
@@ -2,18 +2,13 @@ import { surroundBlockSelection, surroundSelection } from "../editorHelpers";
 import { getEol } from "../env";
 
 const newLine = getEol();
+const blankLine = newLine + newLine;
 
-const wordMatch: string = "[A-Za-z\\u00C0-\\u017F]";
-const startingBlock: string = ">[!BEGINSHADEBOX]" + newLine;
-const endingBlock: string = newLine + ">[!ENDSHADEBOX]";
-const codeBlockWordPattern: RegExp = new RegExp(
-  `${startingBlock}.+${endingBlock}|.+`,
-  "gm"
-);
+const startingBlock: string = ">[!BEGINSHADEBOX]" + blankLine;
+const endingBlock: string = blankLine + ">[!ENDSHADEBOX]" + newLine;
 export function toggleShadebox() {
-  return surroundBlockSelection(
+    return surroundBlockSelection(
     startingBlock,
-    endingBlock,
-    codeBlockWordPattern
+    endingBlock
   );
 }
