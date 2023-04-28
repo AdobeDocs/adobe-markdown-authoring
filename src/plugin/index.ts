a29892c346921e0f1482acf09d570ef6f337469d
George Everitt <geveritt@appliedrelevance.com>
2023-04-27 20:58:05 -0400

    Supports inline and meta badges

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -10,11 +10,13 @@ import { includeFileParts } from "./transform/snippets";
 import { transformTableStyles } from "./transform/table-styles";
 import transformTabs from "./transform/tabs";
 import { transformUICONTROL } from "./transform/uicontrol";
-import { transformBadgeInline } from "./transform/badge";
+import { transformBadgeMeta } from "./transform/badge-meta";
+import { transformBadgeInline } from "./transform/badge-inline";
 import { transformCodeBlock } from "./transform/code-block";
 import { transformHighlights } from "./transform/highlights";
 
 export enum TokenType {
+  FRONT_MATTER = "front_matter",
   BLOCKQUOTE_OPEN = "blockquote_open",
   BLOCKQUOTE_CLOSE = "blockquote_close",
   HTML_BLOCK = "html_block",
@@ -40,6 +42,7 @@ function injectTransforms(md: MarkdownIt, filePath: string) {
   // Now add the token transforms.
   md.core.ruler.after("block", "highlights", transformHighlights);
   md.core.ruler.after("block", "shadebox", transformShadebox);
+  md.core.ruler.after("block", "badge-meta", transformBadgeMeta);
   md.core.ruler.after("block", "badge", transformBadgeInline);
   md.core.ruler.after("block", "link-targets", transformLinkTargets);
   md.core.ruler.after("block", "table-styles", transformTableStyles);
