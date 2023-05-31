5da91077d5ce6eda634c9f2018de05d4e262cb9b
George Everitt <geveritt@appliedrelevance.com>
2023-05-30 20:37:54 -0400

    #42 - Use updated AM009 file

--- src/rules/rules/am009.ts
+++ src/rules/rules/am009.ts
@@ -2,7 +2,6 @@
 
 import {
   addErrorContext,
-  addErrorDetailIf,
   addWarningContext,
   alltags,
   blocktags,
@@ -49,15 +48,17 @@ module.exports = {
     onError: (context: ErrorContext) => void
   ) {
     makeTokenCache(params);
-    var incodeblock: boolean = false;
+    var incodeblock = false;
+
     // check for stray AFM admonitions outside of blockquote or UICONTROL/DNL without brackets
-    for (var i = 0; i < params.lines.length; i++) {
-      // Skip first two lines (0 and 1).
+    var curline = "";
+    for (let i: number = 0; i < params.lines.length; i++) {
       if (i > 1) {
-        var curline = params.lines[i];
+        incodeblock = isInCodeBlock(curline, incodeblock);
+        curline = params.lines[i];
         var prevline = params.lines[i - 1];
         var prevprevline = params.lines[i - 2];
-        incodeblock = isInCodeBlock(curline, incodeblock);
+        // console.log(i + 1, curline)
         if (!incodeblock) {
           if (
             prevprevline.trim().startsWith(">") &&
@@ -67,6 +68,7 @@ module.exports = {
             prevline.trim() !== ""
           ) {
             var warn = false;
+            // console.log(curline, curline.length)
             if (warn) {
               addWarningContext(
                 params.name,
@@ -78,10 +80,9 @@ module.exports = {
                   ' AFM (NOTE) block line missing ">"'
               );
             } else {
-              addErrorDetailIf(
+              addErrorContext(
                 onError,
                 i,
-                "",
                 "",
                 "No > on AFM (NOTE) block line: " + prevline.trim()
               );
@@ -92,29 +93,25 @@ module.exports = {
     }
 
     // check for admonitions in non-comment html block
-    filterTokens(
-      params,
-      "html_block",
-      function forToken(token: MarkdownItToken) {
-        var lines = token.content.split("\n");
-        if (!token.line.startsWith("<!--")) {
-          lines.forEach(function forLine(line: any, lineNumber: any) {
-            if (containsAfmTag(line)) {
-              addErrorDetailIf(
-                onError,
-                lineNumber + token.lineNumber,
-                "",
-                "",
-                "Admonitions not supported in HTML: " + line.trim()
-              );
-            }
-          });
-        }
+    filterTokens(params, "html_block", function forToken(token) {
+      var lines = token.content.split("\n");
+      if (!token.line.startsWith("<!--")) {
+        lines.forEach(function forLine(line: string, lineNumber: number) {
+          if (containsAfmTag(line)) {
+            addErrorContext(
+              onError,
+              lineNumber + token.lineNumber,
+              line,
+              "Admonitions not supported in HTML: " + line.trim()
+            );
+          }
+        });
       }
-    );
-    forEachLine(function forLine(line, lineIndex) {
-      incodeblock = !!isInCodeBlock(line, incodeblock);
+    });
+    forEachLine(function forLine(line: string, lineIndex: number) {
+      incodeblock = isInCodeBlock(line, incodeblock);
       if (!incodeblock) {
+        var origline = line;
         line = line.replace(/`{1,3}.*?`{1,3}/g, " <code> ");
 
         if (!line.match(/^[\s]*\>\[/gm)) {
@@ -167,120 +164,120 @@ module.exports = {
         }
       }
     });
-    filterTokens(
-      params,
-      "blockquote_open",
-      function forToken(token: MarkdownItToken) {
-        var line = token.line;
-        var oline = token.map[0];
-        var cline = token.map[1];
+    filterTokens(params, "blockquote_open", function forToken(token) {
+      var line = token.line;
+      var oline = token.map[0];
+      var cline = token.map[1];
 
-        var indent = token.line.indexOf(">");
-        // console.log(token)
-        for (var i = oline; i < cline; i++) {
-          // console.log(params.lines[i])
-          var lineindent = params.lines[i].indexOf(">");
-          if (lineindent !== indent) {
-            if (lineindent < 0) {
-              addErrorDetailIf(
-                onError,
-                i + 1,
-                indent.toString(),
-                lineindent.toString(),
-                "Missing blockquote marker (>) or newline"
-              );
-            } else {
-              addErrorDetailIf(
-                onError,
-                i + 1,
-                indent.toString(),
-                lineindent.toString(),
-                "Mismatched Indent for Block Quote"
-              );
-            }
-          }
-        }
-        if (token.line.indexOf("[!") > 0) {
-          // is it AFM component
-          // TODO: split the tag out here
-          var afmtag = line.split(/[\[\]]/)[1];
-
-          if (
-            afmtag.includes("!") &&
-            !blocktags.includes(afmtag.replace("!", "")) &&
-            afmtag !== "!VIDEO" &&
-            !afmtag.startsWith("!__BETA")
-          ) {
-            if (
-              afmtag.split(" ").length > 1 &&
-              blocktags_with_options.includes(
-                afmtag.split(" ")[0].replace("!", "")
-              )
-            ) {
-              //good
-            } else {
-              addErrorContext(onError, token.lineNumber, token.line);
-            }
-          }
-          if (!containsAfmTag(token.line)) {
-            addErrorContext(onError, token.lineNumber, token.line);
-          }
-          if (token.line.match(/[\>]*\s+\[!/)) {
-            addErrorContext(onError, token.lineNumber, token.line);
-          }
-          var trimmed = token.line.trim().replace(/\].*$/, "]");
-
-          if (
-            token.line.trim() !== trimmed &&
-            token.line.indexOf("[!VIDEO]") <= 0
-          ) {
-            // check for content after end of the container declaration
-            addErrorContext(onError, token.lineNumber, token.line);
-          }
-
-          if (token.line.indexOf("[!VIDEO]") > 0) {
-            // check for content after the video link
-            trimmed = token.line.trim().replace(/\).*$/, ")");
-            if (token.line.trim() !== trimmed) {
-              addErrorContext(onError, token.lineNumber, token.line);
-            }
-          }
-        } else {
-          var afmtag = line.split(/[\[\]]/)[1];
-
-          // check for afm blocks without ! eg,  >[NOTE]
-          for (var i: number = 0, len = blocktags.length; i < len; i++) {
-            var repattern = "[\\s]*>\\s*\\[" + blocktags[i] + "\\s*\\]";
-            var re = new RegExp(repattern);
-            if (token.line.match(re) !== null) {
-              addErrorContext(onError, token.lineNumber, token.line);
-            }
-          }
-          // check for afm blocks with extra text ! eg,  >[NOTE]
-          for (var i: number = 0, len = blocktags.length; i < len; i++) {
-            var repattern = "[\\s]*>\\s*\\[" + blocktags[i] + "\\s*\\]";
-            var nobang_pattern = "[\\s]*>\\s*\\[!" + blocktags[i] + "\\s*\\]";
-            var textafterafm_pattern =
-              "[\\s]*>\\s*\\[!" + blocktags[i] + "\\s*\\]";
-            var re = new RegExp(repattern);
-            if (token.line.match(re) !== null) {
-              addErrorContext(onError, token.lineNumber, token.line);
-            }
-          }
-          /*
-                    !AFM
-                    AFM
-                */
-          // check for afm tags without ! eg,  [DNL blah]
-          for (var i: number = 0, len = inlinetags.length; i < len; i++) {
-            var repattern = "[\\s]*\\s*\\[" + inlinetags[i] + ".*?\\]";
-            var re = new RegExp(repattern);
-            if (token.line.match(re) !== null) {
-              addErrorContext(onError, token.lineNumber, token.line);
-            }
+      var indent = token.line.indexOf(">");
+      // console.log(token)
+      for (let i = oline; i < cline; i++) {
+        // console.log(params.lines[i])
+        var lineindent = params.lines[i].indexOf(">");
+        if (lineindent != indent) {
+          if (lineindent < 0) {
+            addErrorContext(
+              onError,
+              i + 1,
+              indent.toString(),
+              lineindent,
+              "Missing blockquote marker (>) or newline"
+            );
+          } else {
+            addErrorContext(
+              onError,
+              i + 1,
+              indent.toString(),
+              lineindent,
+              "Mismatched Indent for Block Quote"
+            );
           }
         }
       }
-    );
+      // console.log(token)
+      // console.log(token.lineNumber + params.frontMatterLines.length)
+      // console.log(token.line)
+      // console.log(indent)
+      if (token.line.indexOf("[!") > 0) {
+        // is it AFM component
+
+        // TODO: split the tag out here
+        var afmtag = line.split(/[\[\]]/)[1];
+
+        if (
+          afmtag.includes("!") &&
+          !blocktags.includes(afmtag.replace("!", "")) &&
+          afmtag != "!VIDEO" &&
+          !afmtag.startsWith("!__BETA")
+        ) {
+          if (
+            afmtag.split(" ").length > 1 &&
+            blocktags_with_options.includes(
+              afmtag.split(" ")[0].replace("!", "")
+            )
+          ) {
+            //good
+          } else {
+            addErrorContext(onError, token.lineNumber, token.line);
+          }
+        }
+        if (!containsAfmTag(token.line)) {
+          addErrorContext(onError, token.lineNumber, token.line);
+        }
+        if (token.line.match(/[\>]*\s+\[!/)) {
+          addErrorContext(onError, token.lineNumber, token.line);
+        }
+        var trimmed = token.line.trim().replace(/\].*$/, "]");
+
+        if (
+          token.line.trim() != trimmed &&
+          token.line.indexOf("[!VIDEO]") <= 0
+        ) {
+          // check for content after end of the container declaration
+          addErrorContext(onError, token.lineNumber, token.line);
+        }
+
+        // if (token.line.indexOf('[!VIDEO]') > 0) {
+        //     // check for content after the video link
+        //     trimmed = token.line.trim().replace(/\).*$/, ')')
+        //     if (token.line.trim() != trimmed) {
+        //         addErrorContext(onError, token.lineNumber, token.line);
+        //     }
+        // }
+      } else {
+        var afmtag = line.split(/[\[\]]/)[1];
+
+        // check for afm blocks without ! eg,  >[NOTE]
+        for (var i = 0, len = blocktags.length; i < len; i++) {
+          var repattern = "[\\s]*>\\s*\\[" + blocktags[i] + "\\s*\\]";
+          var re = new RegExp(repattern);
+          if (token.line.match(re) != null) {
+            addErrorContext(onError, token.lineNumber, token.line);
+          }
+        }
+        // check for afm blocks with extra text ! eg,  >[NOTE]
+        for (let i = 0, len = blocktags.length; i < len; i++) {
+          var nobang_pattern = "[\\s]*>\\s*\\[!" + blocktags[i] + "\\s*\\]";
+          var textafterafm_pattern =
+            "[\\s]*>\\s*\\[!" + blocktags[i] + "\\s*\\]";
+          var re = new RegExp(nobang_pattern);
+          if (token.line.match(re) != null) {
+            // addErrorContext(onError, token.lineNumber, token.line);
+          }
+        }
+        /*
+                        !AFM
+                        AFM
+                    */
+        // check for afm tags without ! eg,  [DNL blah]
+        for (let i = 0, len = inlinetags.length; i < len; i++) {
+          var repattern = "[\\s]*\\s*\\[" + inlinetags[i] + ".*?\\]";
+          var re = new RegExp(repattern);
+          if (token.line.match(re) != null) {
+            addErrorContext(onError, token.lineNumber, token.line);
+          }
+        }
+      }
+    });
   },
 };
