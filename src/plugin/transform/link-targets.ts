0db1d25d742e986c581d923b24349076be944c66
George Everitt <geveritt@oproma.com>
2024-06-06 10:30:00 -0400

    Address issue #54 - Preview fails to render link with target =_blank

--- src/plugin/transform/link-targets.ts
+++ src/plugin/transform/link-targets.ts
@@ -4,16 +4,16 @@ import { TokenType } from "..";
 
 export default function transformLinkTargets(state: StateCore) {
   let tokens: Token[] = state.tokens;
-  const targetMatch = /\[([^\]]*)\]\(([^\)]*)\)\{\s*target\s*=\s*([^\}]*)\}/; // [text](url){target=foo}
+  const targetMatch =
+    /(?:^|\s)(\[([^\]]*)\]\(([^\)]*)\)\{\s*target\s*=\s*([^\}]*)\})/; // [text](url){target=foo}
 
   for (var i = 0, l = tokens.length; i < l; i++) {
     let token = tokens[i];
     if (token.type === TokenType.INLINE && token.content.match(targetMatch)) {
       const linkLine = tokens[i].content;
       if (linkLine) {
-        // Convert the linkToken token to an HTML_BLOCK token
         const ids = linkLine.match(targetMatch);
-        const [_, text, url, target] = ids || [];
+        const [_, fullMatch, text, url, target] = ids || [];
 
         const linkToken = new Token("link_open", "a", 1);
         linkToken.attrs = [
@@ -21,12 +21,29 @@ export default function transformLinkTargets(state: StateCore) {
           ["target", target],
         ];
         linkToken.content = text;
-        tokens.splice(i, 1, linkToken); // replace the inline token with the link_open token
+
         const textToken = new Token("text", "", 0);
         textToken.content = text;
-        tokens.splice(i + 1, 0, textToken); // insert the text token
+
         const linkCloseToken = new Token("link_close", "a", -1);
-        tokens.splice(i + 2, 0, linkCloseToken); // insert the link_close token
+
+        const beforeText = linkLine.slice(0, linkLine.indexOf(fullMatch));
+        const afterText = linkLine.slice(
+          linkLine.indexOf(fullMatch) + fullMatch.length
+        );
+
+        const newTokens = [
+          new Token("text", "", 0),
+          linkToken,
+          textToken,
+          linkCloseToken,
+          new Token("text", "", 0),
+        ];
+
+        newTokens[0].content = beforeText;
+        newTokens[4].content = afterText;
+
+        tokens.splice(i, 1, ...newTokens);
       }
     }
   }
