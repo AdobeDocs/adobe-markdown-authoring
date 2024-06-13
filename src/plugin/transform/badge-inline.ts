36e19b63fcbf411e97d9e9b1a111a3941e0fa4e4
Geordie Everitt <geveritt@appliedrelevance.com>
2024-06-13 16:43:20 -0400

    Added exception for "Caution" badge type.

--- src/plugin/transform/badge-inline.ts
+++ src/plugin/transform/badge-inline.ts
@@ -2,49 +2,77 @@ import StateCore from "markdown-it/lib/rules_core/state_core";
 import Token from "markdown-it/lib/token";
 import { TokenType } from "..";
 
+/**
+ * Transforms the inline tokens in the given state to add badge tokens.
+ *
+ * @param {StateCore} state - The state object containing the tokens.
+ */
 export function transformBadgeInline(state: StateCore): void {
+  // Get the tokens from the state
   let tokens: Token[] = state.tokens;
+
+  // Regular expression to match the badge inline tokens
   const badgeRegex = /\[\!BADGE\s(.+?)\](?:\{(.+?)\})?/g;
 
+  // Iterate over the tokens
   for (let i = 0, l = tokens.length; i < l; i++) {
+    // Check if the current token is an inline token
     if (tokens[i].type === TokenType.INLINE) {
+      // Get the content of the token
       let content = tokens[i].content;
+
+      // Find all the badge inline tokens in the content
       let matches = Array.from(content.matchAll(badgeRegex));
 
+      // Iterate over the badge inline tokens
       matches.forEach((match) => {
+        // Extract the label and parameters from the match
         let label = match[1];
         let params = match[2];
+
+        // Default values for type, url, and tooltip
         let type = "Informative";
         let url = "";
         let tooltip = "";
 
         // Check if params exist before processing them
         if (params) {
+          // Split the parameters into key-value pairs
           params.split(/\s+/).forEach((param) => {
             let [key, value] = param.split("=");
+
+            // Assign the parameter values to the corresponding variables
             if (key === "type") {
-              type = value.replace(/['"]/g, ""); // Remove quotes around the type parameter
+              // Remove quotes around the type parameter
+              type = value.replace(/['"]/g, "");
             } else if (key === "url") {
+              // Remove quotes around the url parameter
               url = value.slice(1, -1);
             } else if (key === "tooltip") {
-              tooltip = value.replace(/['"]/g, ""); // Remove quotes around the tooltip parameter
+              // Remove quotes around the tooltip parameter
+              tooltip = value.replace(/['"]/g, "");
             }
           });
         }
 
-        let badgeHTML = `<span class="sp-badge-wrapper"><sp-badge size="s" style="cursor:inherit !important" variant="${type.toLowerCase()}" dir="ltr"`;
+        // Generate the badge HTML
+        let badgeHTML = `<span class="sp-badge-wrapper"><sp-badge size="s" variant="${type.toLowerCase()}" dir="ltr"`;
         if (tooltip) {
           badgeHTML += ` title="${tooltip}"`;
         }
-        badgeHTML += `>${label}</sp-badge></span>`;
-
-        if (url) {
-          badgeHTML = `<a href="${url}" style="color:inherit !important;text-decoration:none">${badgeHTML}</a>`;
+        let style = ' style="cursor:inherit !important;';
+        if (type === "Caution") {
+          style +=
+            " --mod-badge-background-color-default: var(--spectrum-yellow-background-color-default);" +
+            " --mod-badge-label-icon-color-white: var(--spectrum-black);";
         }
+        badgeHTML += `${style}">${label}</sp-badge></span>`;
 
+        // Add the badge HTML to the content
         content = content.replace(match[0], badgeHTML);
       });
 
+      // Update the content of the token
       tokens[i].content = content;
     }
   }
--- src/plugin/transform/badge-meta.ts
+++ src/plugin/transform/badge-meta.ts
@@ -2,21 +2,37 @@ import StateCore from "markdown-it/lib/rules_core/state_core";
 import Token from "markdown-it/lib/token";
 import { TokenType } from "..";
 
+/**
+ * Transforms the meta tokens in the given state to add badge tokens.
+ *
+ * @param {StateCore} state - The state object containing the tokens.
+ */
 export function transformBadgeMeta(state: StateCore): void {
+  // Get the tokens from the state
   const tokens: Token[] = state.tokens;
+
+  // Regular expression to match the "badge" keyword
   const badgeRegex = /^badge/;
+
+  // Regular expression to match key-value pairs within the badge parameters
   const paramRegex = /(\w+)=("[^"]+"|'[^']+'|\S+)/g;
 
+  // Iterate over the tokens
   for (let i = 0, l = tokens.length; i < l; i++) {
+    // Check if the current token is a front matter token
     if (tokens[i].type === TokenType.FRONT_MATTER) {
       const metaToken = tokens[i];
       const metaData = metaToken.meta;
 
+      // Split the meta data into lines
       const metaLines = metaData.split("\n");
+
+      // Iterate over the lines
       for (const line of metaLines) {
         const keyValuePair = line.split(":");
         const key = keyValuePair[0].trim();
 
+        // Check if the key matches the "badge" keyword
         if (key.match(badgeRegex)) {
           const params = keyValuePair[1].trim();
           let matches;
@@ -25,10 +41,12 @@ export function transformBadgeMeta(state: StateCore): void {
           let url = "";
           let tooltip = "";
 
+          // Iterate over the parameters
           while ((matches = paramRegex.exec(params)) !== null) {
             let paramName = matches[1];
             let paramValue = matches[2].replace(/['"]/g, "");
 
+            // Assign the parameter values to the corresponding variables
             if (paramName === "label") {
               label = paramValue;
             } else if (paramName === "type") {
@@ -40,12 +58,19 @@ export function transformBadgeMeta(state: StateCore): void {
             }
           }
 
+          // Generate the HTML for the badge
           let badgeHTML = `<span class="sp-badge-wrapper"><sp-badge size="s" style="cursor:inherit !important" variant="${type.toLowerCase()}" dir="ltr"`;
           if (tooltip) {
             badgeHTML += ` title="${tooltip}"`;
           }
+          if (type === "Caution") {
+            badgeHTML +=
+              " --mod-badge-background-color-default: var(--spectrum-yellow-background-color-default);" +
+              " --mod-badge-label-icon-color-white: var(--spectrum-black);";
+          }
           badgeHTML += `>${label}</sp-badge></span>`;
 
+          // Add the badge HTML to the tokens list
           if (url) {
             badgeHTML = `<a href="${url}" style="color:inherit !important;text-decoration:none">${badgeHTML}</a>`;
           }
